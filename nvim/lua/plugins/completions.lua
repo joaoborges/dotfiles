652d2e381bb1c55ca3cb53c62f5287a5a241ee37
Joao Borges <joaoborges6@gmail.com>
2024-05-22 16:27:44 -0300

    Fixing actions for closing buffers in bufferline and some minor adjustings

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -20,11 +20,7 @@ return {
                 snippet = {
                     -- REQUIRED - you must specify a snippet engine
                     expand = function(args)
-                        --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
                         require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-                        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-                        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-                        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
                     end,
                 },
                 window = {
@@ -40,10 +36,7 @@ return {
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
-                    -- { name = "vsnip" }, -- For vsnip users.
                     { name = "luasnip" }, -- For luasnip users.
-                    -- { name = 'ultisnips' }, -- For ultisnips users.
-                    -- { name = 'snippy' }, -- For snippy users.
                 }, {
                     { name = "buffer" },
                 }),
